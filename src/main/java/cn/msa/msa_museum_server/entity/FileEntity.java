5e4ae46858602afac0fd26f5bd6fe767dc1d3d3a
meow <winter7z@163.com>
2024-11-25 15:01:13 +0800

    Remove jpa dependency

--- src/main/java/cn/msa/msa_museum_server/entity/FileEntity.java
+++ src/main/java/cn/msa/msa_museum_server/entity/FileEntity.java
@@ -1,10 +1,8 @@
 package cn.msa.msa_museum_server.entity;
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Entity
 @Data
 @AllArgsConstructor
 public class FileEntity {
